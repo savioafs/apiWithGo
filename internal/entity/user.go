143982f0a1379f9bd32deb9ab5469cca6baa30fa
Sávio Alexandre <savioalefreitas@gmail.com>
2024-07-22 17:44:16 -0300

    create entity user and genera uuid for pkg

---
+++ internal/entity/user.go
@@ -0,0 +1,38 @@
+package entity
+
+import (
+	"fmt"
+
+	"github.com/savioafs/apiWithGo/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return &User{}, fmt.Errorf("error encrypt password user: %v", name)
+	}
+
+	return &User{
+		ID:       entity.NewID(),
+		Name:     name,
+		Email:    email,
+		Password: string(hashPassword),
+	}, nil
+}
+
+func (u *User) ValidatePassword(password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	if err != nil {
+		return fmt.Errorf("error encrypt password user: %v", u.Name)
+	}
+
+	return nil
+}
