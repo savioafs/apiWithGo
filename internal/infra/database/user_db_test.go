c68e8505cc9c4a3e52b270c323a7421f4010fe6a
Sávio Alexandre <savioalefreitas@gmail.com>
2024-07-22 22:02:08 -0300

    add test find by email user

--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/savioafs/apiWithGo/internal/entity"
@@ -32,4 +33,29 @@ func TestCreateUser(t *testing.T) {
 	as.Equal(userFound.Email, user.Email)
 	as.NotNil(userFound.Password, user.Password)
 
+}
+
+func TestFindByEmail(t *testing.T) {
+	as := assert.New(t)
+
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		as.Error(err)
+	}
+
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("Julia", "julinha@gmail.com", "30312")
+	userDB := NewUser(db)
+
+	err = userDB.Create(user)
+	as.Nil(err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	as.Nil(err)
+	as.Equal(user.ID, userFound.ID)
+	as.Equal(user.Name, userFound.Name)
+	as.Equal(user.Email, userFound.Email)
+	as.NotNil(userFound.Password)
+
+	fmt.Println(user)
 }
