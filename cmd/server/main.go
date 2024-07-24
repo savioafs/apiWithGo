5caa311c573848d9bcdee83bc437bd9f44b861b4
Sávio Alexandre <savioalefreitas@gmail.com>
2024-07-23 21:12:16 -0300

    add handler of product

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 
 	config "github.com/savioafs/apiWithGo/configs"
+	"github.com/savioafs/apiWithGo/internal/entity"
+	"github.com/savioafs/apiWithGo/internal/infra/database"
+	"github.com/savioafs/apiWithGo/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	cfg, err := config.LoadConfig(".")
+	_, err := config.LoadConfig(".")
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)
 	}
 
-	fmt.Println(cfg.DBDriver)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to open db: %v", err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
